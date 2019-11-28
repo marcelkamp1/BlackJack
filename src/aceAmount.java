d4051c4a913e7535a758c7f18f92da4ad1746c00
Martin Langenbacher <martin.langenbacher@googlemail.com>
2019-11-28 18:21:13 +0100

    BlackJack 2.Tag, Teil 2

--- src/aceAmount.java
+++ src/aceAmount.java
@@ -10,6 +10,20 @@ public interface aceAmount {
 	public static void aceAmountPlayer(int aceCounterPlayer, Players player1) {
 		if(aceCounterPlayer == 0);
 			
+		
+		
+		
+		/*
+		 * Info für Marcel ;-)
+		 * 
+		 * 
+		 * 1. Wir bewerten die Asse erst einmal NUR mit dem Wert 1 (eins)
+		 * 2. Dann ermitteln wir die Gesamtsumme
+		 * 3. Ist die Gesamtsumme > 21 geben wir die Summe zurück (als den Wert über 21) und einer hat verloren...k
+		 * 4. ist die Gesamtsumme <=11 (mit mit einem Ass), dann addiere 10
+		 * 
+		 * 
+		 */
 			
 	
 	}
